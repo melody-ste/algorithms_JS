1f1080a49d959ae0d65170d707bdc27c0072d288
melody-ste <melody_stephan@icloud.com>
2025-08-13 16:14:02 +0200

    adding finished exo_6 + modification on 4 for multiple look at the list

--- exo_4.js
+++ exo_4.js
@@ -1,14 +1,22 @@
 function countBuildingsWithWestView(buildings) {
-  let maxHeight = 0; 
-  let count = 0;    
+ let tallestFromRight = [];
 
+  
+  let max = 0;
   for (let i = buildings.length - 1; i >= 0; i--) {
-    if (buildings[i] > maxHeight) {
-      count++;               
-      maxHeight = buildings[i]; 
+    tallestFromRight[i] = max;
+    if (buildings[i] > max) {
+      max = buildings[i];
     }
   }
+
+  let count = 0;
+  for (let i = 0; i < buildings.length; i++) {
+    if (buildings[i] > tallestFromRight[i]) {
+      count++;
+    }
+  }
+
   return count;
 }
-
 console.log(countBuildingsWithWestView([3, 7, 8, 3, 6, 1])); 
